bf1af65f6c1ba71cbb7e5f820b25001eb3502f3b
Ryan Gardner <ryan.gardner@dealer.com>
2015-02-10 21:06:21 -0500

    switch to using a state machine pattern for the ReleaserControl to make it easier to add more functionality / states in the future

--- src/main/java/com/ryebrye/releaser/ReleaserControl.java
+++ src/main/java/com/ryebrye/releaser/ReleaserControl.java
@@ -1,5 +1,7 @@
 package com.ryebrye.releaser;
 
+import com.github.oxo42.stateless4j.StateMachine;
+import com.github.oxo42.stateless4j.StateMachineConfig;
 import org.apache.camel.Consume;
 import org.apache.camel.EndpointInject;
 import org.apache.camel.ProducerTemplate;
@@ -8,8 +10,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 /**
- * This class holds the state of the releaser.
+ * This class manages the state of the releaser and responds to hardware events appropriately.
  * <p/>
+ *
  * @author Ryan Gardner
  * @date 12/31/14
  */
@@ -17,52 +20,80 @@ import org.springframework.stereotype.Component;
 public class ReleaserControl {
     private static final Logger log = LoggerFactory.getLogger(ReleaserControl.class);
 
-    // store the state that we are aware of here.
-    private boolean highSwitchActive = false;
-    private boolean lowSwitchActive = false;
-
-    // this is when we should be in the "drain" state
-    private boolean releasingInProgress = false;
-
     @EndpointInject(uri = "seda:releaserControl")
     ProducerTemplate emptyReleaser;
 
-    @Consume(uri="seda:highSwitchStateChange")
-    public void handleHighSwitchStateChange(boolean state) {
-        if (this.highSwitchActive != state) {
-            this.highSwitchActive = state;
-        }
-        if (this.highSwitchActive) {
-            if (!this.releasingInProgress) {
-                log.info("sending open message");
-                this.releasingInProgress = true;
-                emptyReleaser.sendBody("open");
+    private enum ReleaserState {
+        Filling, Full, Emptying, Empty
+    }
+
+    private enum ReleaserTrigger {
+        SapAboveHighPoint, SapBelowHighPoint, SapAboveLowPoint, SapBelowLowPoint
+    }
+
+    StateMachine<ReleaserState, ReleaserTrigger> releaser;
+
+
+    public ReleaserControl() {
+        StateMachineConfig<ReleaserState, ReleaserTrigger> releaserStateConfig = new StateMachineConfig<>();
+
+        releaserStateConfig.configure(ReleaserState.Filling)
+                .permit(ReleaserTrigger.SapAboveHighPoint, ReleaserState.Full);
+
+        releaserStateConfig.configure(ReleaserState.Full)
+                .onEntry(this::handleFullReleaser)
+                .permit(ReleaserTrigger.SapBelowHighPoint, ReleaserState.Emptying);
+
+        releaserStateConfig.configure(ReleaserState.Emptying)
+                .permit(ReleaserTrigger.SapBelowLowPoint, ReleaserState.Empty);
+
+        releaserStateConfig.configure(ReleaserState.Empty)
+                .onEntry(this::handleEmptyReleaser)
+                .permit(ReleaserTrigger.SapAboveLowPoint, ReleaserState.Filling);
+
+        releaser = new StateMachine<ReleaserState, ReleaserTrigger>(ReleaserState.Empty, releaserStateConfig);
+    }
+
+    private void handleFullReleaser() {
+        log.info("sending open message");
+        emptyReleaser.sendBody("open");
+    }
+
+    private void handleEmptyReleaser() {
+        log.info("releaser is empty, sending message to start it filling again");
+        emptyReleaser.sendBody("close");
+    }
+
+    @Consume(uri = "seda:highSwitchStateChange")
+    public void handleHighSwitchStateChange(boolean highSwitchState) {
+        if (highSwitchState) {
+            if (releaser.canFire(ReleaserTrigger.SapAboveHighPoint)) {
+                releaser.fire(ReleaserTrigger.SapAboveHighPoint);
             } else {
-                log.error("the switch went high again while we were already releasing");
+                log.error("sap above the high point, but that's not valid for the state of {}", releaser.getState().name());
             }
-        }
-        if (!this.highSwitchActive && this.releasingInProgress) {
-            log.info("High switch is not active anymore - draining is working");
+        } else {
+            if (releaser.canFire(ReleaserTrigger.SapBelowHighPoint)) {
+                releaser.fire(ReleaserTrigger.SapBelowHighPoint);
+            } else {
+                log.error("Sap below high point is not a valid trigger for the state of {}", releaser.getState().name());
+            }
         }
     }
 
-    @Consume(uri="seda:lowSwitchStateChange")
-    public void handleLowSwitchStateChange(boolean state) {
-        if (this.lowSwitchActive != state) {
-            this.lowSwitchActive = state;
-        }
-        if (this.releasingInProgress && !this.lowSwitchActive) {
-            this.releasingInProgress = false;
-            if (this.highSwitchActive) {
-                log.error("The high switch is still active - the switch may be malfunctioning.");
+    @Consume(uri = "seda:lowSwitchStateChange")
+    public void handleLowSwitchStateChange(boolean lowSwitchState) {
+        if (lowSwitchState) {
+            if (releaser.canFire(ReleaserTrigger.SapAboveLowPoint)) {
+                releaser.fire(ReleaserTrigger.SapAboveLowPoint);
+            } else {
+                log.error("sap above the high point, but that's not valid for the state of {}", releaser.getState().name());
             }
-            log.info("low switch no longer active - releasing process is complete");
-            // send message to close the releaser
-            emptyReleaser.sendBody("close");
-        }
-        else {
-            if (this.lowSwitchActive && !this.releasingInProgress) {
-               log.info("the low switch is active again - this indicates is filling properly.");
+        } else {
+            if (releaser.canFire(ReleaserTrigger.SapBelowLowPoint)) {
+                releaser.fire(ReleaserTrigger.SapBelowLowPoint);
+            } else {
+                log.error("Sap below high point is not a valid trigger for the state of {}", releaser.getState().name());
             }
         }
     }
